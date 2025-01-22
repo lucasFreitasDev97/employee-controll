866ab1a5c09194a1fbbcfdab0b94957e48c8cc68
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2025-01-22 19:04:31 -0300

    create migration to employees table

---
+++ src/database/migrations/2025_01_22_213929_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birthdate');
+            $table->string('role');
+            $table->string('cpf');
+            $table->string('rg');
+            $table->string('path_to_photo')->nullable();
+            $table->string('phone');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
